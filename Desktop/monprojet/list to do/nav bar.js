055999ecdaf0a0888b93e9b0f6e12fbf4b12f447
raghira-rgb <raghira@gmail.com>
2025-11-12 11:38:50 +0100

    Delete Desktop/monprojet directory

--- Desktop/monprojet/list to do/nav bar.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.querySelector(".navbar");
-const bars = document.querySelector(".fa-bars");
-const xmark = document.querySelector(".fa-xmark");
-const humburguerMenu = document.querySelector(".humburger");
-humburguerMenu.addEventListener("click", () => {
-  bars.classList.toggle("active");
-  xmark.classList.toggle("active");
-  navbar.classList.toggle("active");
-});
